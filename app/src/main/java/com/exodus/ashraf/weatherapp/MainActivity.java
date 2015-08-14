3ad18357a47134847ff05757fa3ea334a0b61cfb
ashrafp725 <ashrafp725@gmail.com>
2015-08-14 19:37:50 +0530

    appbar/toolbar,status bar color

--- app/src/main/java/com/exodus/ashraf/weatherapp/MainActivity.java
+++ app/src/main/java/com/exodus/ashraf/weatherapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.exodus.ashraf.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
